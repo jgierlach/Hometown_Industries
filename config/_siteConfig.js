17c235bd2136de691130be6e9cf4c94e790c8825
jgierlach <jegierlach@gmail.com>
2019-08-21 17:56:18 -0400

    Changed Site Header

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -9,8 +9,8 @@ import theCMS from '../cms/netlify/cms'
  * General Site configurations
  */
 export default {
-  siteName: 'Awake',
-  tagline: 'A Nuxt.js Theme for Netlify CMS',
+  siteName: 'Hometown Industries',
+  tagline: 'Your one stop shop for Amazon solutions',
   featureImage: '/uploads/home-hero.jpg',
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
@@ -33,11 +33,6 @@ export default {
     {
       name: 'Github Repo',
       link: 'https://github.com/danielkellyio/awake-template',
-      target: '_blank'
-    },
-    {
-      name: 'Awake+ Premium',
-      link: 'http://awake-premium-lp.danielkelly.io/',
       target: '_blank'
     }
   ],
